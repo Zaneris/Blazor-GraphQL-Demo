57133709dfd52fefbef2bda3a79fb1b191dea20c
Ted L <dstrike@gmail.com>
2022-11-24 10:21:48 -0500

    First Query - GetAllUsers

--- Program.cs
+++ Program.cs
@@ -8,4 +8,8 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services
+    .AddAdminClient()
+    .ConfigureHttpClient(c => c.BaseAddress = new Uri("https://localhost:7205/graphql"));
+
 await builder.Build().RunAsync();
